e27b8b66465827d4d8f19a041f3717965def6eba
James Tagaca <jtagaca@csub.edu>
2022-05-06 21:33:29 -0700

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Login from "./screens/login";
+import Admin from "./screens/admin";
+import User from "./screens/user";
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Login />} />
-
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/user" element={<User />} />
           {/* <Route exact path="/" component={Login} /> */}
           {/* <Route exact path="/" component={Login} /> */}
         </Routes>
